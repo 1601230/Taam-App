b863add68024f7bfc0486ee560dd7cde6dce8641
1601230 <1601230@uab.cat>
2023-03-22 19:00:54 +0100

    V0.3: Projecte Flutter modificat
    S'ha modificat el metodo _incrementCounter

--- Codi/Frontend Taam App/lib/main.dart
+++ Codi/Frontend Taam App/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:taam_app/requests.dart';
 
 void main() {
   runApp(const MyApp());
@@ -51,14 +52,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  void _incrementCounter() async {
+    int aux = await methodName(_counter);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _counter = aux;
     });
   }
 
