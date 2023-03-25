6c7607ef03fd21c7227ebecbb9a1201e2589b1d2
1601230 <1601230@uab.cat>
2023-03-25 15:01:00 +0100

    V1.2: Modificacions en els fitxers del paquet "Core"
    S'han fet modificacions en el "Taam_App.java" i en el "Searcher.java", les modificacions són: s'han posat comentaris, s'han afegit/eliminat/modificat els atributs, s'han creat els getters necessaris i s'han eliminat les linies de la clase "Main.java" (en principi aquesta classe estar perquè tot funcioni, però no fa res, és a dir, no instància cap cosa).

--- Codi/Backend Taam App/Backend Taam App/src/Core/Searcher.java
+++ Codi/Backend Taam App/Backend Taam App/src/Core/Searcher.java
@@ -1,24 +1,56 @@
 package Core;
 
+/**
+ * The "Searcher" class is the one that allows to query a product (by its name or by its barcode) and an ingredient
+ * (by its name) in the database and retrieve its information. The information retrieved with the methods of this class
+ * are the ones that allow later to check if it is suitable for the user according to his preferences.
+ */
 public class Searcher {
+    private String productName;
+    private Integer barcode;
+    private String ingredientName;
 
     public void Searcher()
     {
-
     }
 
+    /**
+     * This is the search engine for a product by name. This method allows the user to search in the database for the
+     * ingredients of a product by its name.
+     * Attributes "barcode" and "ingredientName" are set to "null" except attribute "productName", in this way we know
+     * at any moment what the user is looking for.
+     */
     public Product searchProductByName(String productName)
     {
+        productName = this.productName;
+        barcode = null;
+        ingredientName = null;
         return null;
     }
-
+    /**
+     * This is the search engine for a product by barcode. This method allows the user to search in the database for the
+     * ingredients of a product by its barcode.
+     * Attributes "productName" and "ingredientName" are set to "null" except attribute "barcode", in this way we know
+     * at any moment what the user is looking for.
+     */
     public Product searchProductByBarcode(Integer barcode)
     {
+        productName = null;
+        barcode = this.barcode;
+        ingredientName = null;
         return null;
     }
-
+    /**
+     * This is the search engine for a ingredient by name. This method allows the user to know if an ingredient is in
+     * the database.
+     * Attributes "productName" and "barcode" are set to "null" except attribute "ingredientName", in this way we know
+     * at any moment what the user is looking for.
+     */
     public boolean searchIngredient(String ingredientName)
     {
+        productName = null;
+        barcode = null;
+        ingredientName = this.ingredientName;
         return false;
     }
 }
--- Codi/Backend Taam App/Backend Taam App/src/Core/Taam_App.java
+++ Codi/Backend Taam App/Backend Taam App/src/Core/Taam_App.java
@@ -1,28 +1,59 @@
 package Core;
 
-import java.sql.Connection;
-
+/**
+ * The "Taam App" class is the main module of the project. It is the class responsible for calling all the necessary
+ * methods to control the execution flow of the application in order to achieve its main purpose. "Taam App" applies
+ * the "singleton" pattern because we are only interested in having one instance of this module, so that each client
+ * uses the same instance during all its requests to the server.
+ */
 public class Taam_App {
     private static Taam_App instance = null;
-    private static Connection conn = null;
-    private static Configuration configuration;
     public static Searcher searcher;
     public static Product product;
     public static Result result;
     public static Recommendations recommendations;
     public static Visitor visitor;
 
+    /**
+     * "Taam App" has a private constructor to create an instance of this class. It is private to prevent uncontrolled
+     * instances from being created, as the aim is that only one instance is created and that the same instance is used at
+     * all times. at all times. From this constructor the connection to the local database is established.
+     */
     private Taam_App()
     {
-
+        ConnectDB.getConnection();
     }
 
-    public static Taam_App getInstance(Connection connection)
+    /**
+     * This method is used to retrieve the unique instance of the class "Taam App" (in case the instance is not created,
+     * it is created using the private constructor).
+     */
+    public static Taam_App getInstance()
     {
         if (instance == null) {
             instance = new Taam_App();
         }
-        conn = connection;
+
         return instance;
+    }
+    public Searcher getSearcher()
+    {
+        return searcher;
+    }
+    public Product getProduct()
+    {
+        return product;
+    }
+    public Result getResult()
+    {
+        return result;
+    }
+    public Recommendations getRecommendations()
+    {
+        return recommendations;
+    }
+    public Visitor getVisitor()
+    {
+        return visitor;
     }
 }
