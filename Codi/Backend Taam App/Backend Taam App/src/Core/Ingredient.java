22b0c5829bc21965f8421dd154789c1209e9ad36
1600611 <1600611@uab.cat>
2023-03-31 15:39:22 +0200

    s'han afegit metodes a la classe Ingredient

--- Codi/Backend Taam App/Backend Taam App/src/Core/Ingredient.java
+++ Codi/Backend Taam App/Backend Taam App/src/Core/Ingredient.java
@@ -1,19 +1,32 @@
 package Core;
 
 public class Ingredient {
-
     private String ingredient;
     private Integer id;
 
-    Ingredient() {}
+    public Ingredient() {
 
+    }
     Ingredient(String ingredient, Integer id)
     {
         this.ingredient = ingredient;
         this.id = id;
     }
 
-    public String getIngredient(){
+    public String getIngredient()
+    {
         return ingredient;
+    }
+    public Integer getId()
+    {
+        return id;
+    }
+    public void setIngredient(String ingredient)
+    {
+        this.ingredient = ingredient;
+    }
+    public void setId(Integer id)
+    {
+        this.id = id;
     }
 }
