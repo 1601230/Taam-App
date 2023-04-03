6226264ed1bc6ac10cec4a75b21fb43a5c5fccdd
Gerard Ruiz <102018322+GerardRuiz002@users.noreply.github.com>
2023-04-03 22:49:02 +0200

    Pagina  de configuración: cambiar tema acabado

--- Codi/Frontend Taam App/lib/page_configuration.dart
+++ Codi/Frontend Taam App/lib/page_configuration.dart
@@ -5,7 +5,9 @@ import 'package:multi_select_flutter/multi_select_flutter.dart';
 //Para crear un StatefulWidget automáticamente, poner stful
 
 class PageConfiguration extends StatefulWidget {
-  const PageConfiguration({Key? key}) : super(key: key);
+  final void Function(Brightness?)? setBrightness;
+  final Brightness brightness;
+  const PageConfiguration({Key? key, required this.setBrightness, required this.brightness}) : super(key: key);
 
   @override
   State<PageConfiguration> createState() => _PageConfigurationState();
@@ -43,97 +45,107 @@ class _PageConfigurationState extends State<PageConfiguration> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             ///Container de cambio de idioma
-            Container(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    margin: EdgeInsets.all(10),
-                    child: const Text(
-                        'Tema de la aplicación',
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
-                    ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: const Text(
+                      'Idioma',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: DropdownButton(
-                      items: _appLanguage.map((String itemsValues) {
-                        return DropdownMenuItem(
-                          value: itemsValues,
-                          child: Text(itemsValues),
-                        );
-                      }).toList(),
-                      value: _selectionLanguageMessage,
-                      onChanged: (String? value) {
-                        setState(() {
-                          _selectionLanguageMessage = value!;
-                        });
-                      },
-                    ),
-                  )
-                ],
-              ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: DropdownButton(
+                    items: _appLanguage.map((String itemsValues) {
+                      return DropdownMenuItem(
+                        value: itemsValues,
+                        child: Text(itemsValues),
+                      );
+                    }).toList(),
+                    value: _selectionLanguageMessage,
+                    onChanged: (String? value) {
+                      setState(() {
+                        _selectionLanguageMessage = value!;
+                      });
+                    },
+                  ),
+                )
+              ],
             ),
             ///Container preferencias alimentarias
-            Container(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    margin: EdgeInsets.all(10),
-                    child: const Text(
-                        'Preferencias alimentarias',
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
-                    ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: const Text(
+                      'Preferencias alimentarias',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: MultiSelectDialogField(
-                      title: Text('Selecciona las preferencias alimentarias'),
-                      buttonText: Text('Selecciona tus preferencias'),
-                      confirmText: const Text('Confirmar'),
-                      cancelText: const Text('Cancelar'),
-                      items: _selectedValuesFoodPreferences.map((option) => MultiSelectItem<String>(option, option)).toList(),
-                      onConfirm: (selectedItems) {
-                        print(selectedItems);
-                      },
-                    )
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: MultiSelectDialogField(
+                    title: Text('Selecciona las preferencias alimentarias'),
+                    buttonText: Text('Selecciona tus preferencias'),
+                    confirmText: const Text('Confirmar'),
+                    cancelText: const Text('Cancelar'),
+                    items: _selectedValuesFoodPreferences.map((option) => MultiSelectItem<String>(option, option)).toList(),
+                    onConfirm: (selectedItems) {
+                      print(selectedItems);
+                    },
                   )
-                ],
-              ),
+                )
+              ],
             ),
             ///Container tema de la aplicación
-            Container(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    margin: EdgeInsets.all(10),
-                    child: const Text(
-                        'Tema de la aplicación',
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
-                    ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: const Text(
+                      'Tema de la aplicación',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: DropdownButton(
-                      items: _appThemeItems.map((String itemsValues) {
-                        return DropdownMenuItem(
-                          value: itemsValues,
-                          child: Text(itemsValues),
-                        );
-                      }).toList(),
-                      value: _selectionThemeMessage,
-                      onChanged: (String? value) {
-                        setState(() {
-                          _selectionThemeMessage = value!;
-                        });
-                      },
-                    ),
-                  )
-                ],
-              ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: DropdownButton(
+                    items: _appThemeItems.map((String itemsValues) {
+                      return DropdownMenuItem(
+                        value: itemsValues,
+                        child: Text(itemsValues),
+                      );
+                    }).toList(),
+                    value: _selectionThemeMessage,
+                    onChanged: (String? value) {
+                      setState(() {
+                        _selectionThemeMessage = value!;
+                        if (_selectionThemeMessage == 'Claro') {
+                          widget.setBrightness!(Brightness.light);
+                        } else {
+                          widget.setBrightness!(Brightness.dark);
+                        }
+                      });
+                    },
+                  ),
+                )
+              ],
             ),
+            ///Container para el botón de confirmación
+            Container(
+              margin: EdgeInsets.all(20),
+              alignment: Alignment.bottomCenter,
+              child: ElevatedButton(
+                child: const Text('Confirmar'),
+                onPressed: () {
+
+                },
+              ),
+            )
           ],
         ),
       ),
