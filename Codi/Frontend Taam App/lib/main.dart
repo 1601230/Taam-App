7c894c5ea26d511bc85f9680fc66ca1825804ac3
Opozo2 <o.p.fernandez4@gmail.com>
2023-04-06 15:33:29 +0200

    Pantalla inicio + Pantalla Buscar Productos v1.0
    La pantalla de inicio se ha implementado en main.dart y se ha creado un drectorio donde pondremos las diferentes pantallas y se ha implementado la pantala de buscar productos

--- Codi/Frontend Taam App/lib/main.dart
+++ Codi/Frontend Taam App/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:taam_app/requests.dart';
 
+import 'pages/search_product_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -40,10 +42,64 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Main',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.brown,
         brightness: _brightness,
       ),
-      home:, ///Pagina inicial (para debugar, podeis poner la pagina que esteis desarollando aqui y asi la podreis visualizar a la hora de ejecutar el emulador) (aqui debera ir a pagina principal de la aplicación)
+      home: MyHomePage(), ///Pagina inicial (para debugar, podeis poner la pagina que esteis desarollando aqui y asi la podreis visualizar a la hora de ejecutar el emulador) (aqui debera ir a pagina principal de la aplicación)
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Spacer(),
+              IconButton(
+                icon: Icon(Icons.settings),
+                onPressed: () {
+
+                  //Aqui se agrega la navegación a la pantalla de Configuración
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 120.0),
+              child: Image.asset(
+                'assets/Logo_TaamApp.png',
+                width: 200,
+                height: 200,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 8, bottom: 100.0),
+              child: Text("El seu selector de menjar de confiança"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context)=> const MySearchProduct())
+                );
+              },
+              child: const Text('INICIAR'),
+            ),
+            const SizedBox(height: 30),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ Codi/Frontend Taam App/lib/pages/search_product_page.dart
@@ -0,0 +1,310 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class MyData {
+  String title;
+  String subtitle;
+
+  MyData({required this.title, required this.subtitle});
+}
+
+final List<MyData> myData = [
+  MyData(title: 'Colacao', subtitle: 'assets/vegan_stamp.png'),
+  MyData(title: 'Alimento 2', subtitle: 'assets/glutenfree_stamp.png'),
+  MyData(title: 'Alimento 3', subtitle: 'assets/vegan_stamp.png'),
+  MyData(title: 'Alimento 4', subtitle: 'assets/Logo_TaamApp.png'),
+  MyData(title: 'Alimento 5', subtitle: 'assets/almento_foto.png'),
+  MyData(title: 'Alimento 8', subtitle: 'assets/Logo_TaamApp.png'),
+  MyData(title: 'Alimento 9', subtitle: 'assets/vegan_stamp.png'),
+];
+
+class MySearchProduct extends StatefulWidget {
+  const MySearchProduct({super.key});
+
+  @override
+  _MySearchProduct createState() => _MySearchProduct();
+}
+
+class _MySearchProduct extends State<MySearchProduct> {
+  var _productName;
+  final _productController = TextEditingController();
+  final _barcodeController = TextEditingController();
+  
+  @override
+  void initState() {
+    super.initState();
+    _productController.addListener(_updateProductText);
+    _barcodeController.addListener(_updateBarcodeText);
+  }
+
+  void _updateProductText(){
+    setState(() {
+      _productName = _productController.text;
+    });
+  }
+
+  void _updateBarcodeText(){
+    setState(() {
+      _productName = _productController.text;
+    });
+  }
+
+  final _focusNodeProduct = FocusNode();
+  final _focusNodeBarcode = FocusNode();
+
+  @override
+  void dispose() {
+    // Limpia el FocusNode cuando el widget se elimina
+    _focusNodeProduct.dispose();
+    _focusNodeBarcode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+    child: Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              const Text(""),
+              IconButton(
+                alignment: Alignment.topCenter,
+                icon: Image.asset('assets/Logo_TaamApp_Home.png'),
+                onPressed: () {
+                  Navigator.pop(context);
+                  // Aqui se agrega la navegación a la pantalla de inicio
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.settings),
+                onPressed: () {
+                  //Aqui se agrega la navegación a la pantalla de Configuración
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Center(
+        child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+                children: [
+                  TextFormField(
+                    controller: _productController,
+                    focusNode: _focusNodeProduct,
+                    decoration: InputDecoration(
+                      hintText: 'Buscar producte/ingredient...',
+                      hintStyle: const TextStyle(color: Colors.grey),
+                      prefixIcon: IconButton(
+                          onPressed: () {
+                            Fluttertoast.showToast(
+                                msg: "Search clicked",
+                                toastLength: Toast.LENGTH_SHORT,
+                                gravity: ToastGravity.CENTER,
+                                timeInSecForIosWeb: 1,
+                                backgroundColor: Colors.red,
+                                textColor: Colors.white,
+                                fontSize: 16.0
+                            );
+                          },
+                          icon: const Icon(Icons.search)),
+                      suffixIcon: _productController.text.isEmpty
+                          ? null
+                          : InkWell(
+                        onTap: () => _productController.clear(),
+                        child: const Icon(Icons.clear),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white70,
+                      contentPadding: EdgeInsets.all(8),
+                      enabledBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        borderSide: BorderSide(color: Colors.grey, width: 2),
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        borderSide: BorderSide(color: Colors.grey, width: 2),
+                      ),
+                    ),
+
+                    keyboardType: TextInputType.text,
+
+                    onEditingComplete: () {
+                      _focusNodeProduct.unfocus();
+
+                      Fluttertoast.showToast(
+                          msg: "Product Name is ${_productController.text}",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.CENTER,
+                          timeInSecForIosWeb: 1,
+                          backgroundColor: Colors.red,
+                          textColor: Colors.white,
+                          fontSize: 16.0
+                      );
+                      _productController.clear(); //Esto limpia el contenido de la barra de búsqueda
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: TextFormField(
+                      controller: _barcodeController,
+                      focusNode: _focusNodeBarcode,
+                      decoration: InputDecoration(
+                        hintText: 'Introduir codi...',
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        prefixIcon: IconButton(
+                          onPressed: () {
+                            Fluttertoast.showToast(
+                                msg: "Camera clicked",
+                                toastLength: Toast.LENGTH_SHORT,
+                                gravity: ToastGravity.CENTER,
+                                timeInSecForIosWeb: 1,
+                                backgroundColor: Colors.red,
+                                textColor: Colors.white,
+                                fontSize: 16.0
+                            );
+                          },
+                          icon: const Icon(Icons.camera_alt)),
+                        suffixIcon: _barcodeController.text.isEmpty
+                          ? null
+                          : InkWell(
+                            onTap: () => _barcodeController.clear(),
+                            child: const Icon(Icons.clear),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white70,
+                        contentPadding: EdgeInsets.all(8),
+                        enabledBorder: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                          borderSide: BorderSide(color: Colors.grey, width: 2),
+                        ),
+                        focusedBorder: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                          borderSide: BorderSide(color: Colors.grey, width: 2),
+                        ),
+                      ),
+                      keyboardType: TextInputType.number,
+                      onEditingComplete: () {
+                        _focusNodeBarcode.unfocus();
+
+                        Fluttertoast.showToast(
+                            msg: "Product Barcode is ${_barcodeController.text}",
+                            toastLength: Toast.LENGTH_SHORT,
+                            gravity: ToastGravity.CENTER,
+                            timeInSecForIosWeb: 1,
+                            backgroundColor: Colors.red,
+                            textColor: Colors.white,
+                            fontSize: 16.0
+                        );
+                        _barcodeController.clear(); //Esto limpia el contenido de la barra de búsqueda
+                      },
+                    )
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                    child: Divider(
+                      height: 10, // Altura de la línea
+                      thickness: 2, // Grosor de la línea
+                      color: Colors.grey, // Color de la línea
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                    child: Text(
+                      "Recomanacions",
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                      child: ListView.builder(
+                        itemCount: myData.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Padding(
+                            padding: EdgeInsets.symmetric(vertical: 4.0),
+                            child: GestureDetector(
+                              onTap: () {
+                                //Añadir navegación a la pantalla del producto
+                                Fluttertoast.showToast(
+                                    msg: "${myData[index].title} Seleccionado",
+                                    toastLength: Toast.LENGTH_SHORT,
+                                    gravity: ToastGravity.CENTER,
+                                    timeInSecForIosWeb: 1,
+                                    backgroundColor: Colors.red,
+                                    textColor: Colors.white,
+                                    fontSize: 16.0
+                                );
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.grey, width: 1.0),
+                                  borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Image.asset(
+                                      myData[index].subtitle,
+                                      width: 100.0,
+                                      height: 100.0,
+                                    ),
+                                    Text(
+                                      myData[index].title,
+                                      style: const TextStyle(
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Column(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Padding(
+                                          padding: EdgeInsets.only(bottom: 2.0, top: 4.0),
+                                          child: Image.asset(
+                                            "assets/vegan_stamp.png",
+                                            width: 33.0,
+                                            height: 33.0,
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.all(2.0),
+                                          child: Image.asset(
+                                            "assets/glutenfree_stamp.png",
+                                            width: 33.0,
+                                            height: 33.0,
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(bottom: 4.0, top: 2.0),
+                                          child: Image.asset(
+                                            "assets/vegan_stamp.png",
+                                            width: 33.0,
+                                            height: 33.0,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      )
+                  )
+                ]
+            )
+        ),
+      ),
+    ),
+    );
+  }
+}
