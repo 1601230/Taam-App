5a0219dd6323fc0894505624eaef85286a0462c0
1601230 <1601230@uab.cat>
2023-04-24 12:03:07 +0200

    Generada la carpeta TEST de backend

---
+++ Codi/Backend Taam App/Backend Taam App/src/Core/test/Core/SearcherTest.java
@@ -0,0 +1,20 @@
+package Core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SearcherTest {
+
+    @Test
+    public void searchProductByBarcode() {
+    }
+
+    @Test
+    public void searchProductByName() {
+    }
+
+    @Test
+    public void searchIngredient() {
+    }
+}
